f545458967bc09c6c9609f8b66c07fb0b834c686
TechnoStudy <ismettechno@gmail.com>
2024-03-06 17:20:52 +0000

    BaseDriver Hazırlandı

---
+++ src/Gun01/_06_Ornek.java
@@ -0,0 +1,13 @@
+package Gun01;
+
+import Utlity.BaseDriver;
+import org.testng.annotations.Test;
+
+public class _06_Ornek extends BaseDriver {
+
+    @Test
+    public void Test1(){
+        driver.get("https://techno.study/");
+    }
+
+}
--- src/Utlity/BaseDriver.java
+++ src/Utlity/BaseDriver.java
@@ -1,43 +1,19 @@
-package Gun01;
+package Utlity;
 
-import Utlity.Tools;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class _05_Enable {
-
-    @Test
-    public void Test1()
-    {
-        System.out.println("Test 1");
-        driver.get("https://techno.study/");
-    }
-
-    @Test
-    public void Test2()
-    {
-        System.out.println("Test 2");
-        driver.get("https://www.facebook.com/");
-    }
-
-    @Test
-    public void Test3()
-    {
-        System.out.println("Test 3");
-        driver.get("https://www.google.com/");
-    }
-
+public class BaseDriver {
     public static WebDriver driver;
     public static WebDriverWait wait;
     @BeforeClass
     public void BaslangicIslemleri(){ // TearStart
-        System.out.println("başlangıç işlemleri yapılıyor"); //driver oluşturma, wait işlemleri,
+       // System.out.println("başlangıç işlemleri yapılıyor"); //driver oluşturma, wait işlemleri,
 
         driver=new ChromeDriver();
 
@@ -50,10 +26,12 @@ public class _05_Enable {
 
     @AfterClass
     public void KapanisIslemleri(){  // TearDown
-        System.out.println("kapanış işlemleri yapılıyor"); //BekleKapat
+        //System.out.println("kapanış işlemleri yapılıyor"); //BekleKapat
 
         Tools.Bekle(3);
         driver.quit();
     }
 
 }
+
+
