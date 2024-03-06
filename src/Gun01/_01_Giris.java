1d6e6b9002f40fcc14f6f1883d6a4c08cded387c
TechnoStudy <ismettechno@gmail.com>
2024-03-06 12:14:48 +0000

    TestNG Start

---
+++ src/Gun01/_01_Giris.java
@@ -0,0 +1,38 @@
+package Gun01;
+
+import org.testng.annotations.Test;
+
+public class _01_Giris {
+
+    //   public static void main(String[] args) {
+//        driverIslemleriniYap();
+//        websitesiniAc();
+//        LoginTestiniYap()
+ //   }
+
+
+//      @Test JUnit Test i
+//      public void Test1()
+//      {
+//          driverIslemleriniYap();
+//          websitesiniAc();
+//          LoginTestiniYap()
+//          Assert
+//      }
+
+     @Test
+     public void driverIslemleriniYap(){
+         System.out.println("driverIslemleriniYap çalıştı");
+     }
+
+    @Test
+    public void websitesiniAc(){
+        System.out.println("websitesiniAc çalıştı");
+    }
+
+    @Test
+    public void LoginTestiniYap(){
+        System.out.println("LoginTestiniYap çalıştı");
+    }
+
+}
