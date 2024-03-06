0217274419d8875fd99d53ed56826a7c908aa334
TechnoStudy <ismettechno@gmail.com>
2024-03-06 18:26:48 +0000

    _08_SubscribeNewsLetter ve Login BeforeClass içine alındı

---
+++ src/Gun01/_08_SubscribeNewsLetter.java
@@ -0,0 +1,21 @@
+package Gun01;
+
+import Utlity.BaseDriver;
+import org.testng.annotations.Test;
+
+/*
+  Senaryo
+  1- Siteyi açınız.
+  2- Newsletter  Subscribe işlemini abone lunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
+
+ */
+
+public class _08_SubscribeNewsLetter extends BaseDriver {
+
+    @Test
+    public void subscribeFunctionYes(){
+
+
+    }
+
+}
