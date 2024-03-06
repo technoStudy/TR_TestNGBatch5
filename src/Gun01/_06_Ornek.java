a7d108c4bd6520ba1a03245a598878c7b5dfa302
TechnoStudy <ismettechno@gmail.com>
2024-03-06 17:38:38 +0000

    LoginTest

--- src/Gun01/_06_Ornek.java
+++ src/Gun01/_06_Ornek.java
@@ -1,13 +1,31 @@
 package Gun01;
 
 import Utlity.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class _06_Ornek extends BaseDriver {
 
     @Test
-    public void Test1(){
-        driver.get("https://techno.study/");
+    public void LoginTest(){
+        System.out.println("Login Test başladı");
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+
+        WebElement email=driver.findElement(By.id("input-email"));
+        email.sendKeys("testng1@gmail.com");
+
+        WebElement password=driver.findElement(By.id("input-password"));
+        password.sendKeys("123qweasd");
+
+        WebElement loginBtn=driver.findElement(By.xpath("//input[@type='submit']")); //By.cssSelector("[value='Login']")
+        loginBtn.click();
+
+        wait.until(ExpectedConditions.titleIs("My Account"));
+        Assert.assertTrue(driver.getTitle().equals("My Account"));
+        System.out.println("Login Test bitti");
     }
 
 }
