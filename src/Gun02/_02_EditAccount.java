4ad1ca96218550283779bace28b303b8c584af53
TechnoStudy <ismettechno@gmail.com>
2024-03-07 17:02:42 +0000

    _02_EditAccount Soru

---
+++ src/Gun02/_02_EditAccount.java
@@ -0,0 +1,11 @@
+package Gun02;
+
+/*
+  Senaryo
+  1- Siteyi açınız.
+  2- Edit Account sayfasına ulaşınız.
+  3- Ad ve soyadı değiştirip tekrar kaydedidiniz.
+ */
+
+public class _02_EditAccount {
+}
