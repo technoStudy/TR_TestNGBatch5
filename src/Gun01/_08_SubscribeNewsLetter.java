6495b57d8c3da5f819fda6e44169acf092b7624a
TechnoStudy <ismettechno@gmail.com>
2024-03-07 16:13:19 +0000

    _08_SubscribeNewsLetter Sadeleştirme ve 3.Soru

--- src/Gun01/_08_SubscribeNewsLetter.java
+++ src/Gun01/_08_SubscribeNewsLetter.java
@@ -1,6 +1,7 @@
 package Gun01;
 
 import Utlity.BaseDriver;
+import Utlity.Tools;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -11,38 +12,44 @@ import org.testng.annotations.Test;
   1- Siteyi açınız.
   2- Newsletter  Subscribe işlemini abone lunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
   3- Ayrı bir test ile Newsletter  Subscribe işlemini abonelikten çıkınız(NO)
+  4- Ayrı bir test ile Newsletter  Subscribe durumunu kontrol ediniz YES ise NO, NO ise YES yapınız.
  */
 
 public class _08_SubscribeNewsLetter extends BaseDriver {
 
+    By link= By.linkText("Newsletter");
+    By subYes = By.xpath("//input[@value='1']");
+    By cntBtn=By.xpath("//input[@value='Continue']");
+    By subNo= By.xpath("//input[@value='0']");
+
     @Test(priority = 1)
     public void subscribeFunctionYes(){
-        WebElement newsLetterLink=driver.findElement(By.linkText("Newsletter"));
+        WebElement newsLetterLink=driver.findElement(link);
         newsLetterLink.click();
 
-        WebElement subscribeYes=driver.findElement(By.xpath("//input[@value='1']"));
+        WebElement subscribeYes=driver.findElement(subYes);
         subscribeYes.click();
 
-        WebElement continueButton=driver.findElement(By.xpath("//input[@value='Continue']"));
+        WebElement continueButton=driver.findElement(cntBtn);
         continueButton.click();
 
-        WebElement msgLabel= driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
-        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+//        WebElement msgLabel= driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+//        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+        Tools.SuccessMessageValidation();
     }
 
     @Test(priority = 2)
     public void subscribeFunctionNo() {
-        WebElement newsLetterLink=driver.findElement(By.linkText("Newsletter"));
+        WebElement newsLetterLink=driver.findElement(link);
         newsLetterLink.click();
 
-        WebElement subscribeNo=driver.findElement(By.xpath("//input[@value='0']"));
+        WebElement subscribeNo=driver.findElement(subNo);
         subscribeNo.click();
 
-        WebElement continueButton=driver.findElement(By.xpath("//input[@value='Continue']"));
+        WebElement continueButton=driver.findElement(cntBtn);
         continueButton.click();
 
-        WebElement msgLabel= driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
-        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+        Tools.SuccessMessageValidation();
     }
 
 }
--- src/Utlity/Tools.java
+++ src/Utlity/Tools.java
@@ -1,5 +1,9 @@
 package Utlity;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
 public class Tools {
 
     public static void Bekle(int sn){
@@ -9,6 +13,11 @@ public class Tools {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void SuccessMessageValidation(){
+        WebElement msgLabel= BaseDriver.driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
     }
 
 }
