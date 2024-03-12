15e2f9b78bfe1979dc10bd471e5ff17e618bf4f2
TechnoStudy <ismettechno@gmail.com>
2024-03-12 18:45:09 +0000

    _06_Soru

---
+++ src/Gun04/_06_Soru.java
@@ -0,0 +1,10 @@
+package Gun04;
+
+/**
+ * Bir önceki taskı PARAMETERDRIVER ile yapınız
+ * sonrasında farklı tarayıcılar ile çalıştırınız. (crossbrowser)
+ * sonrasında paralel çalıştırınız.(parallel)
+ */
+
+public class _06_Soru {
+}
