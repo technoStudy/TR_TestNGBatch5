f6bafb60aaf396ba4bf6431c115c4f868d9a07ed
TechnoStudy <ismettechno@gmail.com>
2024-03-14 18:52:36 +0000

    Ödevler

---
+++ src/Gun06/_04_Soru.java
@@ -0,0 +1,12 @@
+package Gun06;
+
+/*
+  Senaryo
+  1- Siteyi açınız.
+  2- Top Menudeki menu elemanlarının olduğunu doğrulayınız.(menuValidation)
+  3- Desktops, Laptops & Notebooks, Components, Tablets, Software, Phones & PDAs, Cameras, MP3 Players
+ */
+
+
+public class _04_Soru {
+}
